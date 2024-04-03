c7f869d00f9f6c2e81c5dee1cf5bdd0679372fae
Gold Bull <165162257+GitHubGoldUser1@users.noreply.github.com>
2024-04-03 23:53:47 +0530

    Password Generator.py

---
+++ Password Generator.py
@@ -0,0 +1,13 @@
+print("PASSWORD GENERATOR")
+date=input("Enter your date of birth : ")
+name=input("Enter your name : ")
+s=input("Enter any word : ")
+character=input("Enter 10 to Add more 10 character or Enter 20 to Add more 20 character or Enter 30 to Add more 30 character :   ")
+if character=="10":
+    print("Ah9ft",date,"ZY",name,"Mge",s)
+elif character=="20":
+    print("Zshgjiuymk",date,"Gh5iu",name,"kdfuy",s)
+elif character=="30":
+    print("Puihjokg9frtyhg",date,"683kL",name,"Mhkasdfgcv",s)
+else:
+    print("lkgdcvxdghfghhycdv67",date,"8tfhufr97",name,"ljgsdfhc57888",s)
