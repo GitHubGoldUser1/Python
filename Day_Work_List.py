498f809255563b990846de8b57c481d91b7c984b
GOLD BULL <165162257+GitHubGoldUser1@users.noreply.github.com>
2024-11-07 18:47:19 +0530

    Day_Work_List.py
    A simple code to make a list of work which have to complete in a Day

---
+++ Day_Work_List.py
@@ -0,0 +1,100 @@
+print("DAY WORK LIST")
+print("Enter Password to start Program")
+Pw=input("")
+if Pw=="password":
+     print("Enter 1 to add task")
+     print("Enter 2 to view task")
+     print("Enter 3 to Remove task")
+     tsk=input("")
+     if tsk=="1":
+       tsk1=input("Enter task:  ")
+     else:
+       print("Enter Wrong Character")
+     if tsk=="2":
+       print("No task Enter")
+     if tsk=="3":
+       tsk="No task"
+     else:
+       print("")
+     print("Enter 1 to add task")
+     print("Enter 2 to view task")
+     print("Enter 3 to Remove task")
+     tsk=input("")
+     if tsk=="1":
+       tsk1=input("Enter task:  ")
+     else:
+         print("")
+     if tsk=="2":
+         print(tsk1)
+     else:
+         print("")  
+     if tsk=="3":
+         print("Task Removed")
+     else:
+         print("Thank You")
+     print("Enter 1 to add task")
+     print("Enter 2 to view task")
+     print("Enter 3 to Remove task")
+     tsk=input("")
+     if tsk=="1":
+       tsk1=input("Enter task:  ")
+     else:
+         print("")
+     if tsk=="2":
+         print(tsk1)
+     else:
+         print("")  
+     if tsk=="3":
+         print("Task Removed")
+     else:
+         print("Thank You")
+     print("Enter 1 to add task")
+     print("Enter 2 to view task")
+     print("Enter 3 to Remove task")
+     tsk=input("")
+     if tsk=="1":
+       tsk1=input("Enter task:  ")
+     else:
+         print("")
+     if tsk=="2":
+         print(tsk1)
+     else:
+         print("")  
+     if tsk=="3":
+         print("Task Removed")
+     else:
+         print("Thank You")
+     print("Enter 1 to add task")
+     print("Enter 2 to view task")
+     print("Enter 3 to Remove task")
+     tsk=input("")
+     if tsk=="1":
+       tsk1=input("Enter task:  ")
+     else:
+         print("")
+     if tsk=="2":
+         print(tsk1)
+     else:
+         print("")  
+     if tsk=="3":
+         print("Task Removed")
+     else:
+         print("Thank You")
+     print("Enter 1 to add task")
+     print("Enter 2 to view task")
+     print("Enter 3 to Remove task")
+     tsk=input("")
+     if tsk=="1":
+       tsk1=input("Enter task:  ")
+     else:
+         print("")
+     if tsk=="2":
+         print(tsk1)
+     else:
+         print("")  
+     if tsk=="3":
+         print("Task Removed")
+     else:
+         print("Thank You")
+else:
+   print("InValid Password")
